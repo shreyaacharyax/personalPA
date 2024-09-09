19fab8aa3eec001f2e489a42686dc7c37c929a2e
Shreya <shreya.acharya2710@gmail.com>
2024-09-09 17:36:51 +0545

    Fix Firebase setup

--- app/firebase.js
+++ app/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// lib/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA7PTj4e_f1qiTDxZLjN7DnEldYeNreKMQ",
   authDomain: "customer-ai-29bb5.firebaseapp.com",
@@ -17,10 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-  
-  const auth = firebase.auth();
-  export { auth };
-  
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
